a2aca38ed45987313f1cd8248b85f34d435d1e00
kandelmilan <kandelr950p@gmail.com>
2025-09-14 18:27:27 +0545

    Added

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require("express")
-const app=express()
-const UserRouter=require("./router/User.router")
+const app = express()
+const UserRouter = require("./router/User.router")
 
 
-app.use(express.json());  
+app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.status(200).send("Server is Serving")
 })
 
-app.use("/api/v1",UserRouter)
+app.use("/api/v1", UserRouter)
 
 
-module.exports=app
+module.exports = app
