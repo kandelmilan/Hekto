23073fb2b925afafffa120b1e7cb82976f099f1a
kandelmilan <kandelr950p@gmail.com>
2025-09-15 09:48:18 +0545

    Added:BannerRoute

---
+++ src/controller/Banner.controller.js
@@ -0,0 +1,46 @@
+const banner = require("../model/Banner.model")
+const Joi = require("joi")
+
+
+const bannerValidationSchema = Joi.object({
+    title: Joi.string()
+        .trim()
+        .required()
+        .messages({
+            "string.base": "Title must be a required",
+            "string.empty": "Title cannot be empty",
+        }),
+    image: Joi.string()
+        .uri()
+        .required()
+        .messages({
+            "string.base": "Image must be a string",
+            "string.empty": "Image is required",
+            "string.uri": "Image must be a valid URL"
+        }),
+    description: Joi.string()
+        .optional()
+        .messages({
+            "string.base": "Description must be a string"
+        }),
+    discount: Joi.number()
+        .min(0)
+        .optional()
+        .messages({
+            "number.base": "Discount must be a number",
+            "number.min": "Discount cannot be negative"
+        }),
+    link: Joi.string()
+        .uri()
+        .optional()
+        .messages({
+            "string.uri": "Link must be a valid URL"
+        }),
+    subtitle: Joi.string()
+        .optional()
+        .messages({
+            "string.base": "Subtitle must be a string"
+        })
+});
+
+module.exports = { bannerValidationSchema };
